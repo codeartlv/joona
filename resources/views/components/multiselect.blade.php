ad1d79aa882f6d5213a83a7ec13a427c9d8fd392
Deniss Kozlovs <deniss@codeart.lv>
2024-09-04 12:57:35 +0300

    Add group to select/multiselect

--- resources/views/components/multiselect.blade.php
+++ resources/views/components/multiselect.blade.php
@@ -7,16 +7,36 @@
 		<em data-role="selected-text"></em>
 	</div>
 	<ul class="dropdown-menu">
-		@foreach ($options as $option)
-			<li>
-				<x-checkbox
+		@foreach ($options as $item)
+			@if (method_exists($item, 'options'))
+				@if ($item->selectable)
+					<li class="form-multiselect__group">
+						<x-checkbox :checked="$item->selected" :disabled="$item->disabled" label="{{$item->label}}" value="{{$item->value}}" name="{{ $name }}"/>
+					</li>
+				@else
+					<li class="form-multiselect__group-label">{{$item->label}}</li>
+				@endif
+
+				@php
+					$entries = $item->options();
+				@endphp
+			@else
+				@php
+					$entries = [$item];
+				@endphp
+			@endif
+
+			@foreach ($entries as $option)
+				<li>
+					<x-checkbox
 					:checked="$option->selected"
 					:disabled="$option->disabled"
 					label="{{$option->label}}"
 					value="{{$option->value}}"
 					name="{{ $name }}"
 				/>
-			</li>
+				</li>
+			@endforeach
 		@endforeach
 	</ul>
 </div>
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,7 +2,15 @@
 	<label class="form-label {{$required ? 'required':''}}">{{$label}}</label>
 @endif
 <select class="form-select {{$size ? 'form-select-'.$size:''}}" {{ $attributes }}>
-	@foreach ($options as $option)
-		<option value="{{$option->value}}" {{$option->disabled ? 'disabled':''}} {{$option->selected?'selected':''}}>{{$option->label}}</option>
+	@foreach ($options as $item)
+		@if (method_exists($item, 'options'))
+			<optgroup label="{{$item->label}}">
+				@foreach ($item->options() as $option)
+					<option value="{{$option->value}}" {{$option->disabled ? 'disabled':''}} {{$option->selected?'selected':''}}>{{$option->label}}</option>
+				@endforeach
+			</optgroup>
+		@else
+			<option value="{{$item->value}}" {{$item->disabled ? 'disabled':''}} {{$item->selected?'selected':''}}>{{$item->label}}</option>
+		@endif
 	@endforeach
 </select>
